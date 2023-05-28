92e8e2209bc5f2d53de175fb7d6e0890b3d494fe
Faisal Al-Rayes <faisalalrayyess@icloud.com>
2023-05-28 17:50:03 +0200

    changed the name of the env variables to match the refactor.

--- web_project/web/Django_Project/MyWebsite/settings/production.py
+++ web_project/web/Django_Project/MyWebsite/settings/production.py
@@ -11,11 +11,11 @@ ALLOWED_HOSTS = ["localhost", "web", "proxy"]
 DATABASES = {
     "default": {
         "ENGINE": config("DATABASE_ENGINE", default="django.db.backends.sqlite3"),
-        "NAME": config("DATABASE_NAME", default=os.path.join(BASE_DIR, "db.sqlite3")),
-        "USER": config("DATABASE_USER", default="user"),
-        "PASSWORD": config("DATABASE_PASSWORD", default="password"),
+        "NAME": config("PG_DJ_DATABASE", default=os.path.join(BASE_DIR, "db.sqlite3")),
+        "USER": config("PG_DJ_USER", default="user"),
+        "PASSWORD": config("PG_DJ_PASSWORD", default="password"),
         "HOST": config("DATABASE_HOST", default="localhost"),
-        "PORT": config("DATABASE_PORT", default="1234")
+        "PORT": config("PGPORT", default="1234")
     }
 }
 
