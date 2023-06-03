477b47bcce36494b22f53d5826d7e33127c33a68
Faisal Al-Rayes <faisalalrayyess@icloud.com>
2023-06-03 20:03:50 +0200

    dynamic creation of the django superuser

--- web_project/web/scripts/migrate.sh
+++ web_project/web/scripts/migrate.sh
@@ -1,4 +1,14 @@
 #!/bin/bash
 
 # Migrate to the database.
-/opt/venv/bin/python /home/django/my-website-project/manage.py migrate --noinput
+/opt/venv/bin/python /home/django/my-website-project/manage.py migrate --no-input
+
+# Create the django superuser.
+/opt/venv/bin/python /home/django/my-website-project/manage.py createsuperuser --no-input || true
+
+# Release the sensitive data that is no longer needed to reduce the area of exposure of this information.
+# In this case it doesn't really do much if someone with knowlege about django gets access to the container.
+# However it would make their life a little more difficult .
+unset DJANGO_SUPERUSER_USERNAME
+unset DJANGO_SUPERUSER_PASSWORD
+unset DJANGO_SUPERUSER_EMAIL
